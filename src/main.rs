35715e46ecdc4d28260faecaa1aa969cf3770ac0
Jackson Wain Desktop <jwai0004@student.monash.edu>
2021-12-14 12:37:27 +1100

    Type aliases parsed successfully

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use tree_sitter::{Parser, Language};
+use tree_sitter::{Parser, Language, Query, QueryCursor};
 
 use std::path::Path;
 use std::fs::read_to_string;
 
 extern "C" {fn tree_sitter_haskell() -> Language; }
+
 
 fn main() {
     let mut parser = Parser::new();
@@ -11,28 +12,39 @@ fn main() {
     let language = unsafe { tree_sitter_haskell() };
     parser.set_language(language).unwrap();
 
-    // let source_fn = Path::new("test.hs");
-    // let source_code = read_to_string(source_fn).unwrap();
+    let source_path = Path::new("test.hs");
+    let source_code = read_to_string(source_path).unwrap();
+    let source = source_code.as_bytes();
+
+    // let source = "type TestTuple a b = (a, b)".as_bytes();
+    // let source = "type TestVariable a = Maybe a".as_bytes();
+
+
+    let tree = parser.parse(source, None).unwrap();
+
+    let query = "(type_alias) @alias";
+
+    // let type_aliases_sexp = "(type_alias name: (type) @alias_lhs (type_name (type) @alias_rhs))";
+    // let type_list_sexp = "(type_alias name: (type) @list_lhs (type_list (type_name (type))) @list_rhs)";
+    // let query = format!("{} {}", type_aliases_sexp, type_list_sexp);
+    let get_type_aliases = Query::new(language, &query).unwrap();
     
-    // let source_code = "x = 1";
-    let source_code = b"type FilePath = String";
+    let mut query_cursor = QueryCursor::new();
 
-    let tree = parser.parse(source_code, None).unwrap();
+    let matches = query_cursor.matches(&get_type_aliases, tree.root_node(), source);
 
-    let new_type_name = tree.root_node().child(0).unwrap().child(1).unwrap().utf8_text(source_code).unwrap();
+    let all_matches = matches.flat_map(|m| m.captures);
+    // let strings = all_matches.map(|m| m.node.to_sexp());
+    let strings = all_matches.map(|m| m.node.child(1).unwrap().utf8_text(source).unwrap());
+
+
+    // let new_type_name = tree.root_node().child(0).unwrap().child(1).unwrap().utf8_text(source).unwrap();
 
     println!("{}", tree.root_node().to_sexp());
-    println!("{}", new_type_name);
 
+    for string in strings {
+        println!("{}", string);
+        
+    }
 }
 
-
-
-
-// use tree_sitter_parsers::parse;
-
-
-// fn main() {
-//     let tree = parse("type Memes = String", "haskell");
-//     println!("{}", tree.root_node().to_sexp());
-// }
--- test.hs
+++ test.hs
@@ -1,2 +1,6 @@
-type FilePath = String
-type Name = [Char]
+type TestAlias = String
+type TestVariable a = Maybe a
+type TestList = [Char]
+type TestTuple a b = (a, b)
+
+
